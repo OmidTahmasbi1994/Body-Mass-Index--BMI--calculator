cd920e4f190b57f98c1249862ae96b00e1424f92
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-19 17:01:40 +0330

    Add files via upload

---
+++ Body Mass Index (BMI) calculator.py
@@ -0,0 +1,32 @@
+# Body Mass Index (BMI) calculator
+"""
+
+     Body Mass Index (BMI) calculator :
+
+
+"""
+
+
+w = float(input('weight in kilo :'))    #For Example: 69 kilo
+h = float(input('height in meter :'))   #For Example: 1.75 meter
+bmi = w/h**2
+
+if bmi < 15 :
+    k = 'Very Severely Underweight'
+elif 15 <= bmi <= 16 :
+    k = 'Severely Underweight'
+elif 16 < bmi <= 18.5 :
+    k = 'Underweight'
+elif 18.5 < bmi <= 25 :
+    k = 'Normal'
+elif 25 < bmi <= 30 :
+    k ='Overweight'
+elif 30 < bmi <= 35 :
+    k = 'Moderately Obese'
+elif 35 < bmi <= 40 :
+    k = 'Severely Obese'
+elif bmi > 40 :
+    k = 'Very Severely Obese !!! '
+    
+    
+print('Your Body Mass Index Is = ' + str(bmi) +' '+'And You Are'+' ' + k +' .')
